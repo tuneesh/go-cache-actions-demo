9bbc80ce6bce419e9501b53f03f5132d74e3b70e
tlella <tlella@cisco.com>
2026-09-16 18:53:43 -0700

    Add same-size cache safety check

--- api/api_test.go
+++ api/api_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestVersion(t *testing.T) {
 	time.Sleep(2 * time.Second)
-	if Version() != "version-3" {
+	if Version() != "version-X" {
 		t.Fatalf("Version() = %q, want version-3", Version())
 	}
 }
